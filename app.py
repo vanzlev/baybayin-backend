5703bec308efdf8c62aa81a12dbcc98703873430
vanzlev <vanzlev25@gmail.com>
2025-02-20 22:56:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def ocr():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ != "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
